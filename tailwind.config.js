57424a93a151cc02b3dc4a63675eae78c115a23b
duva-ha <dvhai.vi@gmail.com>
2026-05-14 21:27:10 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Đảm bảo mọi đường dẫn đều là tương đối
-})
